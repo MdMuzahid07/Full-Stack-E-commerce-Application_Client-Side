e2b375960cb8e60cf7feb17f8dd180031a6dbb29
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-11-10 19:55:31 +0600

    tailwind and daiyui install

---
+++ src/pages/home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='bg-indigo-500 min-h-screen'>
+            <h2>homepage</h2>
+        </div>
+    );
+};
+
+export default Home;
