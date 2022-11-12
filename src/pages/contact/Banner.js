14cc9a9e319c9be2dbc4e5a261ac5461164c6355
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-11-12 17:22:24 +0600

    contact page UI updated

--- src/pages/contact/Banner.js
+++ src/pages/contact/Banner.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 const Banner = () => {
     return (
-        <div className="border rounded-xl bg-black p-10 md:px-20 my-10">
+        <div className="border rounded-xl bg-white p-10 md:px-20 my-10">
             <div className="md:flex justify-between items-center gap-4">
                 <div class="flex items-center">
                     <div>
                         <Link to="/">
-                            <span className="text-2xl text-white mr-2">Home <span className="font-bold">{`<`}</span></span>
+                            <span className="text-2xl mr-2">Home <span className="font-bold">{`<`}</span></span>
                         </Link>
                     </div>
                     <div>
