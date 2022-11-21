c29c35dbc43874218df9fbbba3ba716d04f6a8c9
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-11-21 10:08:24 +0600

    review slider card maked, and responsive optimized

--- src/components/review/ReviewSlider.js
+++ src/components/review/ReviewSlider.js
@@ -11,7 +11,7 @@ const ReviewSlider = () => {
         autoplaySpeed: 5000,
         cssEase: "linear",
         speed: 500,
-        slidesToShow: 4,
+        slidesToShow: 3,
         slidesToScroll: 1,
         swipeToSlide: true,
         initialSlide: 0,
@@ -19,14 +19,14 @@ const ReviewSlider = () => {
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
+                    slidesToShow: 2,
                     slidesToScroll: 1,
                     infinite: true,
                     dots: false
                 }
             },
             {
-                breakpoint: 800,
+                breakpoint: 900,
                 settings: {
                     slidesToShow: 2,
                     slidesToScroll: 1,
@@ -55,32 +55,97 @@ const ReviewSlider = () => {
             <Slider arrows={false} {...settings}>
 
                 <div className="p-2">
-                    <div className="bg-white border rounded-xl p-3">
-                        <h3>1</h3>
+                    <div className="bg-white border-2 hover:border-warning rounded-xl p-4">
+                        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque voluptatibus provident vel maxime, distinctio, culpa mollitia quam quis reprehenderit ratione veniam odio iusto adipisci error et voluptatem reiciendis aut ducimus veritatis. Repellat adipisci iure, recusandae quae rem reprehenderit officia eaque aliquam totam doloremque perspiciatis autem cum fuga aliquid sint deserunt.</span>
+                    </div>
+                    <div className="mt-5 flex items-center">
+                        <div className="avatar">
+                            <div className="avatar">
+                                <div className="w-16 rounded-full">
+                                    <img src="https://placeimg.com/192/192/people" alt="profile_image" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="ml-3">
+                            <p><small>CEO OF PineApple</small></p>
+                            <p className="text-xl">John doe</p>
+                        </div>
                     </div>
                 </div>
 
                 <div className="p-2">
-                    <div className="bg-white border rounded-xl p-3">
-                        <h3>1</h3>
+                    <div className="bg-white border-2 hover:border-warning rounded-xl p-4">
+                        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque voluptatibus provident vel maxime, distinctio, culpa mollitia quam quis reprehenderit ratione veniam odio iusto adipisci error et voluptatem reiciendis aut ducimus veritatis. Repellat adipisci iure, recusandae quae rem reprehenderit officia eaque aliquam totam doloremque perspiciatis autem cum fuga aliquid sint deserunt.</span>
+                    </div>
+                    <div className="mt-5 flex items-center">
+                        <div className="avatar">
+                            <div className="avatar">
+                                <div className="w-16 rounded-full">
+                                    <img src="https://placeimg.com/192/192/people" alt="profile_image" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="ml-3">
+                            <p><small>CEO OF PineApple</small></p>
+                            <p className="text-xl">John doe</p>
+                        </div>
                     </div>
                 </div>
 
                 <div className="p-2">
-                    <div className="bg-white border rounded-xl p-3">
-                        <h3>1</h3>
+                    <div className="bg-white border-2 hover:border-warning rounded-xl p-4">
+                        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque voluptatibus provident vel maxime, distinctio, culpa mollitia quam quis reprehenderit ratione veniam odio iusto adipisci error et voluptatem reiciendis aut ducimus veritatis. Repellat adipisci iure, recusandae quae rem reprehenderit officia eaque aliquam totam doloremque perspiciatis autem cum fuga aliquid sint deserunt.</span>
+                    </div>
+                    <div className="mt-5 flex items-center">
+                        <div className="avatar">
+                            <div className="avatar">
+                                <div className="w-16 rounded-full">
+                                    <img src="https://placeimg.com/192/192/people" alt="profile_image" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="ml-3">
+                            <p><small>CEO OF PineApple</small></p>
+                            <p className="text-xl">John doe</p>
+                        </div>
                     </div>
                 </div>
 
                 <div className="p-2">
-                    <div className="bg-white border rounded-xl p-3">
-                        <h3>1</h3>
+                    <div className="bg-white border-2 hover:border-warning rounded-xl p-4">
+                        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque voluptatibus provident vel maxime, distinctio, culpa mollitia quam quis reprehenderit ratione veniam odio iusto adipisci error et voluptatem reiciendis aut ducimus veritatis. Repellat adipisci iure, recusandae quae rem reprehenderit officia eaque aliquam totam doloremque perspiciatis autem cum fuga aliquid sint deserunt.</span>
+                    </div>
+                    <div className="mt-5 flex items-center">
+                        <div className="avatar">
+                            <div className="avatar">
+                                <div className="w-16 rounded-full">
+                                    <img src="https://placeimg.com/192/192/people" alt="profile_image" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="ml-3">
+                            <p><small>CEO OF PineApple</small></p>
+                            <p className="text-xl">John doe</p>
+                        </div>
                     </div>
                 </div>
 
                 <div className="p-2">
-                    <div className="bg-white border rounded-xl p-3">
-                        <h3>1</h3>
+                    <div className="bg-white border-2 hover:border-warning rounded-xl p-4">
+                        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque voluptatibus provident vel maxime, distinctio, culpa mollitia quam quis reprehenderit ratione veniam odio iusto adipisci error et voluptatem reiciendis aut ducimus veritatis. Repellat adipisci iure, recusandae quae rem reprehenderit officia eaque aliquam totam doloremque perspiciatis autem cum fuga aliquid sint deserunt.</span>
+                    </div>
+                    <div className="mt-5 flex items-center">
+                        <div className="avatar">
+                            <div className="avatar">
+                                <div className="w-16 rounded-full">
+                                    <img src="https://placeimg.com/192/192/people" alt="profile_image" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="ml-3">
+                            <p><small>CEO OF PineApple</small></p>
+                            <p className="text-xl">John doe</p>
+                        </div>
                     </div>
                 </div>
 
