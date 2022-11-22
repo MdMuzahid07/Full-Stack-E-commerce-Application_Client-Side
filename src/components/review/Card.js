1b682083e03bb5bb3bf7f693f21def43e0366458
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-11-22 16:27:50 +0600

    review card height fixed

--- src/components/review/Card.js
+++ src/components/review/Card.js
@@ -4,19 +4,23 @@ const Card = () => {
     return (
         <div className="p-2">
             <div className="bg-white border-2 hover:border-warning rounded-xl p-4">
-                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque voluptatibus provident vel maxime, distinctio, culpa mollitia quam quis reprehenderit ratione veniam odio iusto adipisci error et voluptatem reiciendis aut ducimus veritatis. Repellat adipisci iure, recusandae quae rem reprehenderit officia eaque aliquam totam doloremque perspiciatis autem cum fuga aliquid sint deserunt.</span>
+                <div style={
+                    {
+                        maxHeight: "110px",
+                    }
+                }> <small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque voluptatibus provident vel maxime, distinctio, culpa mollitia quam quis reprehenderit ratione veniam odio iusto adipisci error et voluptatem reiciendis aut ducimus veritatis</small> </div>
             </div>
-            <div className="mt-5 flex items-center">
+            <div className="mt-3 flex items-center">
                 <div className="avatar">
                     <div className="avatar">
-                        <div className="w-16 rounded-full">
+                        <div className="w-11 rounded-full">
                             <img src="https://placeimg.com/192/192/people" alt="profile_image" />
                         </div>
                     </div>
                 </div>
                 <div className="ml-3">
                     <p><small>CEO OF PineApple</small></p>
-                    <p className="text-xl">John doe</p>
+                    <p className="text-lg">John doe</p>
                 </div>
             </div>
         </div>
